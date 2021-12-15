47359250ccd5733372ec529ae1439bcdfcfa600b
rovan89 <devireland89@gmail.com>
2021-12-15 15:52:59 +0000

    Add SCOPE API list

---
+++ run.py
@@ -0,0 +1,8 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
