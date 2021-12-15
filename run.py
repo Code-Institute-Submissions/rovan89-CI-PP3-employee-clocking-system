7b1880bad90fa90fd968ccba51b7581549ea58b2
rovan89 <devireland89@gmail.com>
2021-12-15 15:57:58 +0000

    Add constant variables to allow access to the employee_clocking_system spreadsheet

--- run.py
+++ run.py
@@ -6,3 +6,8 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('employee_clocking_system')
