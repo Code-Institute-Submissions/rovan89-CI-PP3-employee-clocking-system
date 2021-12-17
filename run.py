2d6cd6841c07a28764eeb404557d208e3fa997b4
rovan89 <devireland89@gmail.com>
2021-12-17 14:11:22 +0000

    Add function to create a list of current employee numbers

--- run.py
+++ run.py
@@ -30,9 +30,6 @@ employeeList = [ {
     }
 ]
 
-#print(employeeList)
-#print(employeeList[0]["employeeNumber"])
-
 def options_menu():
     """ 
     Gets chosen option from employee to clock in or out
@@ -40,45 +37,57 @@ def options_menu():
     print("Please choose one of the following options:\n 1. Clock in \n 2. Clock out")
     options = input("Please enter the number that corresponds with the option you would like to choose: ")
 
-
+#Employee validation
 
 list_of_numbers = []
 def list_of_employees_numbers():
     """
     Itterates throught employees numbers.
     """
-    list_of_numbers =  [num["employeeNumber"] for num in employeeList]
+    for num in employeeList:
+        list_of_numbers.append(num["employeeNumber"])
     return list_of_numbers
 
-def validate_employee_number(values):
+def validate_employee_number_count(values):
     """
     Rasises ValueError if value is not an int.
     Checks if there is exactly 3 values.
     Checks if this is a valid employee number.
     """
+    list_of_employees_numbers()
     try:
         if len(values) != 3:
             raise ValueError(
                 f"3 values are required, you provided {len(values)}"
             )
-        return True
+        return False
+
         """
         this part of the function is not working. 
         Try to add functionality to check if the user enerted a correct user number.
         """
-        for i in list_of_numbers:
-            print(i)
-            if values != i:
-                raise ValueError(
-                    f"This is an incorrect employee number"
-                )
-        return True
 
     except ValueError as e:
         print(f"Invalid entry: {e}, please try again\n ")
         return False
     
-    return False
+    return True
+
+def validate_employee_number(values):
+    for i in list_of_numbers:
+        if values != [i]:
+            raise ValueError(
+                f"This is an incorrect employee number"
+               )
+        return False
+
+#for i in list_of_numbers:
+ #       if values != [i]:
+  #          raise ValueError(
+   #             f"This is an incorrect employee number"
+    #           )
+     #   print([i])
+
 
 def employee_clock_in():
     """
@@ -86,16 +95,22 @@ def employee_clock_in():
     """
     while True:
         employee_number = input("Please enter you employee number: ")
+        validate_employee_number_count(employee_number)
         validate_employee_number(employee_number)
 
-        if validate_employee_number(employee_number):
+        if validate_employee_number_count(employee_number):
             print("Valid entry!")
             break
 
+
+#employee_number = input("Please enter you employee number: ")
+#validate_employee_number(employee_number)
+
 def main():
-    print(list_of_employees_numbers()) 
+    print(list_of_numbers) 
+    list_of_employees_numbers()
     options_menu()
-    employee_clock_in()
-    print(list_of_employees_numbers()) 
+    #employee_clock_in()
+    print(list_of_numbers) 
 
 main()
